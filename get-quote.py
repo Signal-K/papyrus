a7e030e5cccba09dbbcc43805390bc5e8c2e7023
Liam Arbuckle <allianceofdroidsla@gmail.com>
2020-05-26 13:47:13 +0800

    #3
    Co-Authored-By: Liam Arbuckle <liam@acordsoftware.tech>

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
 def primary():
-  print("Keep it logically awesome.")
+  # print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  # print(quotes[0])
+  print(quotes[13])
 
   if __name__== "__primary__":
     primary()
