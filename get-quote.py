4862075d4e4f4a925c958c6e25dac3040be0f512
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-06-20 18:13:23 +0800

    #4 #3 #2 #1
    Add random quote, so it's now random
    
    Repl.it link: https://repl.it/@IrisDroidology/python-random-quote
    
    https://github.com/irisdroidology/python-learning/issues/3

--- get-quote.py
+++ get-quote.py
@@ -1,3 +1,4 @@
+import random # imports the "random" python module that allows us to choose random numbers
 def primary():
   # print("Keep it logically awesome.")
 
@@ -6,7 +7,9 @@ def primary():
   f.close()
 
   # print(quotes[0])
-  print(quotes[13])
+  last = 13 # We have 14 (index values) quotes in quotes.txt, so the possible random values (see below line) are 0-13, or the 1st quote to the 14th
+  rnd = random.randint(0, last) 
+  print(quotes[rnd])
 
   if __name__== "__primary__":
     primary()
