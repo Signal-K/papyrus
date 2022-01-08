2e606581a0f24be61337a878dc235dab586ec653
Gizmotronn <droidology@allianceofdroids.org.au>
2022-01-08 17:55:31 +1100

    👋📭 Starting to fix #7 #8

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -1,75 +1,68 @@
 import React from 'react';
 import GoogleLogin from 'react-google-login';
 import { useNavigate } from 'react-router-dom';
-import { FcGoogle } from 'react-icons/fc'; // Google Logo
+import { FcGoogle } from 'react-icons/fc';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
 
-// Assets
-import shareVideo from './assets/share.mp4';
-import logo from './assets/logowhite.png';
-
-import { client } from '../client'; // Sanity client
+import { client } from '../client';
 
 const Login = () => {
-    const navigate = useNavigate();
-    const responseGoogle = (response) => {
-        localStorage.setItem('user', JSON.stringify(response.profileObj));
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    localStorage.setItem('user', JSON.stringify(response.profileObj));
+    const { name, googleId, imageUrl } = response.profileObj;
+    const doc = {
+      _id: googleId,
+      _type: 'user',
+      userName: name,
+      image: imageUrl,
+    };
+    client.createIfNotExists(doc).then(() => {
+      navigate('/', { replace: true });
+    });
+  };
 
-        const  { name, googleId, imageUrl } = response.profileObj; // Set these values to the local storage
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className=" relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
 
-        // Create a new document (user) based on these values for the Sanity user schema
-        const doc = {
-            _id: googleId,
-            _type: 'user', // type of schema/doc to create
-            userName: name,
-            image: imageUrl,
-        }
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" />
+          </div>
 
-        client.createIfNotExists(doc)
-            .then(() => {
-                navigate('/', { replace: true }); // Redirect to the home page after user logs in
-            })
-    }
-
-    return (
-        <div className="flex justify-start items-center flex-col h-screen">
-            <div className="relative w-full h-full">
-                <video 
-                    src={shareVideo}
-                    type="video/mp4"
-                    loop
-                    controls={false}
-                    muted
-                    autoPlay
-                    className='w-full h-full object-cover'
-                />
-
-                <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
-                    <div className="p-5">
-                        <img src={logo} width="130px" alt="logo" />
-                    </div>
-
-                    <div className="shadow-2xl">
-                        <GoogleLogin
-                            clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
-                            render={(renderProps) => (
-                                <button
-                                    type="button"
-                                    class="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                                    onClick={renderProps.onClick}
-                                    disabled={renderProps.disabled} // disable button if disabled prop is true
-                                >
-                                    <FcGoogle className="mr-4" /> Sign in with Google
-                                </button>
-                            )}
-                            onSuccess={responseGoogle} // If we can click the button, load the Google auth [as a] response
-                            onFailure={responseGoogle} 
-                            cookiePolicy="single_host_origin"
-                        />
-                    </div>
-                </div>
-            </div>
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
